19ffeffaded6af3367632a28fabdb7e62de11f72
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-07 01:05:24 +0700

    init template

--- app.vue
+++ app.vue
@@ -1,5 +1,26 @@
 <template>
-  <NuxtWelcome></NuxtWelcome>
+  <i-layout>
+    <i-layout-header
+      ><i-navbar>
+        <i-navbar-brand to="/"> Simulasi Biaya Balik Nama </i-navbar-brand>
+        <i-navbar-collapsible>
+          <i-nav>
+            <i-nav-item disabled to="/"> Halaman Depan </i-nav-item>
+            <i-nav-item disabled to="/about"> Tentang Kami </i-nav-item>
+            <i-nav-item disabled to="/contact"> Kontak </i-nav-item>
+          </i-nav>
+          <i-input disabled placeholder="Ketik untuk mencari di sini...">
+            <template #append>
+              <i-button color="primary">
+                <i-icon name="ink-search" />
+              </i-button>
+            </template>
+          </i-input>
+        </i-navbar-collapsible> </i-navbar
+    ></i-layout-header>
+    <i-layout-content class="p-4"><SimpleTable></SimpleTable></i-layout-content>
+    <i-layout-footer class="text-center text-slate-400/75"></i-layout-footer>
+  </i-layout>
 </template>
 
 <style lang="scss">
---
+++ components/SimpleTable.vue
@@ -0,0 +1,75 @@
+<template>
+  <i-input
+    v-model="hargaBeli"
+    placeholder="Ketikkan Harga Beli Mobil Anda..."
+  />
+  <i-table border class="mt-4">
+    <thead>
+      <tr>
+        <th></th>
+        <th></th>
+      </tr>
+    </thead>
+    <tbody class="text-sm">
+      <tr>
+        <th scope="row">Bea balik nama mobil</th>
+        <td class="text-right">Rp0</td>
+      </tr>
+      <tr>
+        <th scope="row">Pajak kendaraan bermotor</th>
+        <td class="text-right">Rp0</td>
+      </tr>
+      <tr>
+        <th scope="row">SWDKLLJ</th>
+        <td class="text-right">Rp{{ swdkllj.toLocaleString("id-ID") }}</td>
+      </tr>
+      <tr>
+        <th scope="row">Biaya administrasi STNK</th>
+        <td class="text-right">
+          Rp{{ biayaAdministrasiStnk.toLocaleString("id-ID") }}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Biaya penerbitan STNK</th>
+        <td class="text-right">
+          Rp{{ biayaPenerbitanStnk.toLocaleString("id-ID") }}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Penerbitan BPKB</th>
+        <td class="text-right">
+          Rp{{ penerbitanBpkb.toLocaleString("id-ID") }}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Pendaftaran</th>
+        <td class="text-right">Rp{{ pendaftaran.toLocaleString("id-ID") }}</td>
+      </tr>
+      <tr>
+        <th scope="row">Penerbitan TNKB</th>
+        <td class="text-right">
+          Rp{{ penerbitanTnkb.toLocaleString("id-ID") }}
+        </td>
+      </tr>
+      <tr>
+        <th scope="row">Total Biaya</th>
+        <td class="text-right">
+          <span class="font-bold"
+            >Rp{{ totalBiaya.toLocaleString("id-ID") }}</span
+          >
+        </td>
+      </tr>
+    </tbody>
+  </i-table>
+</template>
+
+<script setup>
+const hargaBeli = ref(null);
+const swdkllj = ref(143000);
+const biayaAdministrasiStnk = ref(50000);
+const biayaPenerbitanStnk = ref(200000);
+const penerbitanBpkb = ref(350000);
+const pendaftaran = ref(100000);
+const penerbitanTnkb = ref(100000);
+const totalBiaya = ref(0);
+</script>
