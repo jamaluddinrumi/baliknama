5ab2630f9d2a4a8c0ec73a4d5033f60e4986ca3b
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-06 23:32:07 +0700

    init

---
+++ app.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <NuxtWelcome />
+  </div>
+</template>
+
+<style lang="scss">
+@import "@inkline/inkline/css/variables";
+@import "@inkline/inkline/css/mixins";
+
+:root {
+  --color--primary--h: 195deg;
+  --color--primary--s: 77%;
+  --color--primary--l: 39%;
+}
+</style>
