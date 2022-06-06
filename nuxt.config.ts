c22beff18ab2844fc07c12656bf84d8932008116
Jamaluddin Rumi <jamal.rumi@icloud.com>
2022-06-07 00:06:08 +0700

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@ import { defineNuxtConfig } from "nuxt";
 
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ["@inkline/nuxt"],
+  modules: ["@nuxtjs/tailwindcss", "@inkline/nuxt"],
   inkline: {
     // Plugin options (optional)
   },
